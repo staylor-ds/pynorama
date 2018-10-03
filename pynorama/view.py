08fff1afb4ec468ac71dea5efdf6d09f899d8fc6
Luke Miller <13883308+lukerm@users.noreply.github.com>
2018-10-03 17:37:05 +0100

    upon error, additionally print the traceback

--- pynorama/view.py
+++ pynorama/view.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 from .exceptions import ViewNotFound
 from .logging import logger
 from six import iteritems
-
+from traceback import print_tb
 
 views = OrderedDict()
 
@@ -18,7 +18,8 @@ def register_view(view):
     try:
         view.load()
     except Exception as e:
-        logger.error('Error loading {}: {}'.format(view.get_name(), e))
+        logger.error('Error loading {}: {}, with traceback:'.format(view.get_name(), e))
+        print_tb(e.__traceback__)
 
 
 def get_view(name):
